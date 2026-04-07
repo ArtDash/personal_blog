0b44d92ca1ec4df27c5cccdf9623a99e875af455
ArtDash <belousovgeorge@gmail.com>
2026-04-07 23:58:45 +0300

    Quartz sync: Apr 7, 2026, 11:58 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Belousov",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
